9b63d3f18e1b218b83ed75ea7ee09fdd084083cc
Hà Viết Tráng <trang12596@gmail.com>
2016-10-13 21:04:18 +0700

    Add files via upload

---
+++ src/database/connectDatabase.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author HAVIETTRANG
+ */
+public class connectDatabase {
+    void connect(){
+        
+    }
+    
+    void disconnect(){
+        
+    }
+}
